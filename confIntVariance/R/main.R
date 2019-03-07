509a0d161fd295677f3d9f76ffd60a3f5f676410
mathias <matthew887.mm@gmail.com>
2019-03-07 20:16:23 +0100

    added several equivalent formulas for the variance estimator of the sample variance, all give the same result

--- confIntVariance/R/main.R
+++ confIntVariance/R/main.R
@@ -17,6 +17,16 @@ lsepvs <- function(x) {
 
 
 					# least square unbiased estimator of the variance of the usual unbiased sample variance
+
+                                        # least-squares unbiased (and therefore unique, thus "the") estimator of E[(X-m)^4], the centralized but non-standardized kurtosis
+kurt <- function(x) {
+    m <- mean(x)
+    c2 <- mean((x - m)^2)
+    c4 <- mean((x - m)^4)
+    n <- length(x)
+    (3/2/(n-1) + 6/(n-2) - 27/2/(n-3)) * c2^2 + (1+(1/(n-1)-6/(n-2)+9/(n-3))) * c4
+}
+
 					# equals the U-statistic but in a computationally efficient form
 varianceOfSampleVariance <- function(x) {
     v <- var(x)
@@ -25,6 +35,25 @@ varianceOfSampleVariance <- function(x) {
     					# the first term estimates its expectation, the second term the square of the expectation of the unbiased sample variance, i.e., the square of the population variance
     var(x)^2 - lsepvs(x)
 }
+
+                                        # alternative way to estimate the variance of the sample variance, closer to the formula kurt/n - sigma^4*(n-3)/n/(n-1) from the literature, for instance Cramer.
+                                        # gives the same result as the preceding function
+varianceOfSampleVariance2 <- function(x) {
+    n <- length(x)
+    kurt(x)/n - lsepvs(x)*(n-3)/n/(n-1)
+}
+
+
+                                        # yet another formula for the same object, gives the same result
+varianceOfSampleVariance3 <- function(x) {
+    m <- mean(x)
+    c2 <- mean((x - m)^2)
+    c4 <- mean((x - m)^4)
+    n <- length(x)
+    ( (3/2/(n-1) + 6/(n-2) - 27/2/(n-3))/n -  (1 + 1/2/(n-1) + 5/2/(n-2) + 9/2/(n-2)/(n-3)) * (n-3)/n/(n-1)) * c2^2 + ( (1+(1/(n-1)-6/(n-2)+9/(n-3)))/n+(1/(n-2) + 3/(n-2)/(n-3))
+*(n-3)/n/(n-1)) * c4
+}
+
 
                                         # the confidence interval for the population variance around the usual unbiased sample variance
 					# using as standard deviation the square of the estimated variance of the usual unbiased sample variance, as estimated in the preceding function
